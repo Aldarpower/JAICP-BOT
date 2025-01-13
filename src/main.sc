1ac4cfdc2a38eac4e04529a303a40afda13aa972
aldarpowerhouse@gmail.com <aldarpowerhouse@gmail.com>
2025-01-13 12:36:30 +0000

    Edited files:     /src/main.sc     /src/prod.js

--- src/main.sc
+++ src/main.sc
@@ -1,23 +1,36 @@
+require: prod.js
 require: slotfilling/slotFilling.sc
   module = sys.zb-common
+
 theme: /
 
     state: Start
         q!: $regex</start>
-        a: Начнём.
+        a: Приветствую, я могу найти необходимые вам товары.
 
-    state: Hello
-        intent!: /привет
-        a: Привет привет
+    state: Searchф
+        intent!: /Поиск
+        script:
+            // Извлекаем ключевую часть запроса
+            $session.query = $request.query.replace(/(нужны|)/gi, "").trim();
+            $session.query = $session.query.charAt(0).toUpperCase() + $session.query.slice(1); // Делаем первую букву заглавной
+        a: Вы хотите найти "{{$session.query}}"?
+        event: confirm
+
+    state: Confirm
+        intent!: /Согласие
+        
+        a: Вcе что нашел по запросу "{{$session.query}}"
+        script:
+            searchprod($session.query);
+    state: Decline
+        intent!: /нет
+        a: Хорошо, если вам что-то понадобится, просто дайте знать!
 
     state: Bye
         intent!: /пока
-        a: Пока пока
+        a: Пока-пока!
 
     state: NoMatch
         event!: noMatch
         a: Я не понял. Вы сказали: {{$request.query}}
-
-    state: Match
-        event!: match
-        a: {{$context.intent.answer}}
---
+++ src/prod.js
@@ -0,0 +1,45 @@
+function fetchProducts(query) {
+    var out = $http.get("https://real-time-product-search.p.rapidapi.com/search?country=ru&q=" + encodeURIComponent(query) + "&language=ru&page=1&limit=3&sort_by=BEST_MATCH&product_condition=ANY&min_rating=ANY", {
+        headers: {
+            'x-rapidapi-key': 'a7359e1b8emsh73a5a4e297b19b5p1e2a53jsnedcab4f7b3ac', // Замените на ваш ключ
+            'x-rapidapi-host': 'real-time-product-search.p.rapidapi.com'
+        },
+        timeout: 10000
+    });
+    return out;
+}
+
+// Функция для вывода товаров с пагинацией
+function showProductPage(page, products) {
+    var pageSize = 3;
+
+    // Вычисляем индекс начала и конца текущей страницы
+    var startIndex = page * pageSize;
+    var endIndex = Math.min(startIndex + pageSize, products.length);
+
+    var message = "";
+
+    // Формируем сообщение с товарами для текущей страницы
+    products.slice(startIndex, endIndex).forEach(function (product) {
+        message += "- **" + product.product_title + "**\n" +
+                   "  Рейтинг: " + product.product_rating + "\n" +
+                   "  Цена: " + product.offer.price + "\n" +
+                   "  [Купить здесь](" + product.offer.offer_page_url + ")\n\n";
+    });
+
+    $reactions.answer(message);
+}
+
+function searchprod(query){
+    var g = query;
+            fetchProducts(g).then(function(response) {
+                if (response.status === "OK") {
+                    var products = response.data.products;
+                    showProductPage(0, products); // Показываем первую страницу
+                } else {
+                    $reactions.answer("Ошибка при получении данных: " + response.data.status);
+                }
+            }).catch(function(error) {
+                $reactions.answer("Ошибка сети: " + error.message);
+            });
+}
